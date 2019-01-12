e45dd9824da81c0431f400c743a954a413b3659d
Atul Singh <atul23571@gmail.com>
2019-01-12 17:30:41 +0530

    step filter

--- src/controllers/ScenarioController.js
+++ src/controllers/ScenarioController.js
@@ -13,7 +13,11 @@ import Request from '../models/Request';
 import ParamSetting from '../models/ParamSetting';
 const ignoredExt = ['css', 'jpeg', 'jpg', 'png', 'js', 'woff2', 'gif', 'PNG', 'JPG', 'JPEG', 'GIF', 'JS', 'GIF', 'woff', 'svg'];
 const ignoredUrls = ['www.google-analytics.com', 'www.facebook.com', 'www.fb.com', 'www.youtube.com', 'maps.google.com', 'www.google.com',
-'www.google.co.in','googleads.g.doubleclick.net', 'accounts.google.com', 'www.googletagmanager.com', 'stats.g.doubleclick.net','apis.google.com'];
+'www.google.co.in','googleads.g.doubleclick.net', 'accounts.google.com', 'www.googletagmanager.com', 'stats.g.doubleclick.net','apis.google.com',
+"static.licdn.com",
+"www.linkedin.com",
+"platform.linkedin.com"];
+
 
 import config from '../config';
 
