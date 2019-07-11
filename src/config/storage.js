b1431b61df6e0d3910e901637796d7e30a34fa17
Atul Singh <atul23571@gmail.com>
2019-07-11 12:43:49 +0530

    updatd with env

--- src/config/storage.js
+++ src/config/storage.js
@@ -4,7 +4,7 @@ const storage = {
   path: env("JMX_PATH", "/Users/atul/webroot/testingtool/jmx/"),
   jtlPath: env("JTL_PATH", "/Users/atul/webroot/perfeasy/jtl/"),
   csvPath: env("CSV_PATH", "/Users/atul/webroot/testingtool/csv/"),
-  sampleCsvPath: env("SAMPLE_SCV", "/Users/atul/webroot/perfeasy/sample_csv/"),
+  sampleCsvPath: env("CSV_SAMPLE", "/Users/atul/webroot/perfeasy/sample_csv/"),
   csvInstruction: env("CSV_INSTRUCTION", "/Users/atul/webroot/perfeasy/csv_instruction/"),
   temp: env("TEMP_PATH", "")
 };
